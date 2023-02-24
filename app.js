5409423c1ada04d7e7411ae097fc7301fcd67fe1
Israel Brassell <israel.brassell@gmail.com>
2023-02-23 20:33:31 -0500

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const inquirer = require('inquirer');
 const express = require('express');
 const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:password@127.0.0.1:3306/employees_db')
+
+const Employee = sequelize.define('employee', {
+    name: Sequelize.STRING,
+    jobTitle: Sequelize.STRING,
+    department: Sequelize.STRING,
+    salary: Sequelize.FLOAT,
+    manager: Sequelize.STRING
+  });
